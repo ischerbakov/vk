6dee39ba0ba55e77f963be2f87609a6aaa07a5a5
Игорь Щербаков <i.v.shcherbakov@gmail.com>
2016-02-27 06:38:12 +0600

    initial commit

---
+++ vk.js
@@ -0,0 +1,95 @@
+var vk = new (require('vksdk'))({
+  appId: 1,
+  https: true,
+  version: '5.45',
+})
+
+function copy(from, to) {
+  for (var i in from)
+    to[i] = from[i]
+}
+
+function merge(map1, map2) {
+  var map = {}
+  copy(map1, map)
+  copy(map2, map)
+  return map
+}
+
+function date(seconds) {
+  var pad = s => (s+='').length < 2 ? '0'+s : s
+  var d = new Date(seconds*1000)
+  
+  var year  = d.getFullYear()
+  var date  = d.getDate()
+  var month = d.getMonth() + 1
+  
+  return year+'-'+pad(month)+'-'+pad(date)
+}
+
+var proto = (module.exports = function(locale, domain) {
+  this.domain = domain
+  this.locale = locale
+}).prototype
+
+proto.params = {
+  version: '5.45',
+  method: 'wall.get',
+  owner: '1',
+}
+
+proto.map = {
+  ru: domain => 'ru.'+domain,
+}
+
+proto.domainLocal = function(domain) {
+  var locale = this.locale || ''
+  var map = this.map[locale] || (domain => domain)
+  return map(domain || this.domain)
+}
+
+proto.request = function(params, listener) {
+  if ("undefined" === listener) {
+    listener = domain
+    domain = false
+  }
+  
+  params = merge(this.params, params)
+  params.domain = this.domainLocal(params.domain)
+  
+  vk.request('wall.get', params, listener)
+  return vk
+}
+
+proto.get = function(params, listener, filter) {
+  self = this
+  function call(result) {
+    copy({ domain: self.domainLocal(params.domain) }, result)
+    listener(result)
+  }
+  function error(text, e) {
+    call({ error: { text: text, instance: e } })
+  }
+  copy({ vesion: '5.45' }, params)
+  var req = this.request(params, function(res) {
+    if (res.error || !res.response)
+      return error('VK error')
+    if (!res.response.items)
+      return error('no items')
+    
+    filter = filter || (item => item)
+    defalt = item => filter(merge(item, {
+      date: date(item.date)
+    }))
+    
+    var items = []
+    for (var i in res.response.items)
+      items.push(defalt(res.response.items[i]))
+    call({ items: items })
+  })
+  for (var error in ['http-error', 'parse-error'])
+    req.on(error, e => error(error, e))
+  
+  return req
+}
+
